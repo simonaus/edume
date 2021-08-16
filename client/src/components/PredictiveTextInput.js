5ba2f5eab9f25e34b4c6a4aaba0203a64bf745df
Simon <simonrosenberg1@gmail.com>
2021-08-16 10:25:31 +0100

    Feat: Add warning when input is invalid

--- client/src/components/PredictiveTextInput.js
+++ client/src/components/PredictiveTextInput.js
@@ -3,19 +3,36 @@ import { useState } from 'react';
 export const PredictiveTextInput = () => {
 
   const [input, setInput] = useState('');
+  const [warning, setWarning] = useState('');
 
   function submitHandler(e) {
     e.preventDefault();
+
+    if (input.match(new RegExp(/[^2-9]/))) {
+      setWarning('* Input must only contain digits 2-9');
+      return;
+    }
+
+    setWarning('');
     setInput('');
   }
 
   return (
     <div>
+      <div style={styles.warning}>{warning}</div>
       <form onSubmit={submitHandler}>
-        <input type='text' value={input} placeholder="Input digits here" onChange={(e) => { setInput(e.target.value) }} />
+        <input type='text' value={input} placeholder="Input digits (2-9) here" onChange={(e) => { setInput(e.target.value) }} />
         <br />
         <button type="submit">Submit</button>
       </form>
     </div>
   );
+}
+
+const styles = {
+  warning: {
+    color: '#8B0000',
+    fontWeight: 'bold',
+    fontSize: '.7rem',
+  }
 }
