476cff6bba48f47e8a3181cc2f7fc0307604e840
Simon <simonrosenberg1@gmail.com>
2021-08-16 17:48:23 +0100

    Test: Add test for 1 number being provided

--- server/generateWordsFromNumbers.test.js
+++ server/generateWordsFromNumbers.test.js
@@ -1,2 +1,16 @@
 const generateWordsFromNumbers = require('./generateWordsFromNumbers');
 
+describe('generateWordsFromNumbers function', () => {
+
+  test('returns empty array if no numbers are provided', () => {
+    expect(generateWordsFromNumbers([])).toEqual([]);
+  })
+
+  test('returns correct array of letters if one number is provided', () => {
+    expect(generateWordsFromNumbers(['2'])).toEqual(['a', 'b', 'c']);
+  })
+
+  test('returns correct array of letters if multiple number is provided', () => {
+    expect(generateWordsFromNumbers(['2', '3'])).toEqual(['ad', 'bd', 'cd', 'ae', 'be', 'ce', 'af', 'bf', 'cf']);
+  })
+})
