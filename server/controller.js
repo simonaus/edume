148b942b0a4e5e91326a74f8171e517f6667b750
Simon <simonrosenberg1@gmail.com>
2021-08-16 15:54:35 +0100

    Feat: Create number to letters T9 mapping

---
+++ server/controller.js
@@ -0,0 +1,13 @@
+const generateSuggestedWords = (req, res) => {
+
+  const numbersArray = req.body.split('');
+  let wordsArray = [];
+
+  for (let number in numbersArray) {
+
+  }
+
+  return wordsArray;
+}
+
+module.exports = generateSuggestedWords;
