37535156af17b87c5672919dcaa07373a7be61ca
Simon <simonrosenberg1@gmail.com>
2021-08-16 10:37:53 +0100

    Feat: Create Word component

---
+++ client/src/components/Word.js
@@ -0,0 +1,16 @@
+export const Word = (props) => {
+
+  return (
+    <div style={styles.container}>
+      <div>
+        {props.text}
+      </div>
+    </div>
+  )
+}
+
+const styles = {
+  container: {
+    border: '1px solid #bbbbbb',
+  }
+}
