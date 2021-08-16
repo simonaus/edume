ed952733a2beed320c3e26f193e9f8211bd30cbd
Simon <simonrosenberg1@gmail.com>
2021-08-16 17:55:24 +0100

    Feat: Add error state if word generator results in an error

--- server/controller.js
+++ server/controller.js
@@ -1,10 +1,15 @@
 const generateWordsFromNumbers = require('./generateWordsFromNumbers');
 
 const getPredictedText = (req, res) => {
-  const numbersArray = req.params.query.split('');
-  const suggestedWords = generateWordsFromNumbers(numbersArray).sort();
-  res.status(200).send(suggestedWords);
-  return;
+  try {
+    const numbersArray = req.params.query.split('');
+    const suggestedWords = generateWordsFromNumbers(numbersArray).sort();
+    res.status(200).send(suggestedWords);
+    return;
+  } catch {
+    res.status(500).send('Internal server error: Server unable to process the request at this time');
+    return;
+  }
 }
 
 module.exports = getPredictedText;
