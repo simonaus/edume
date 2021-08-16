f18d9b191379011a68ff641ca71cfba5b169189a
Simon <simonrosenberg1@gmail.com>
2021-08-16 17:09:55 +0100

    Fix: Server now returns words in alphabetical order

--- server/controller.js
+++ server/controller.js
@@ -2,7 +2,7 @@ const generateWordsFromNumbers = require('./generateWordsFromNumbers');
 
 const getPredictedText = (req, res) => {
   const numbersArray = req.params.query.split('');
-  const suggestedWords = generateWordsFromNumbers(numbersArray);
+  const suggestedWords = generateWordsFromNumbers(numbersArray).sort();
   res.status(200).send(suggestedWords);
   return;
 }
