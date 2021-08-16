b18b2c3b328a38ef2aa4d8960eede5286f27a5ef
Simon <simonrosenberg1@gmail.com>
2021-08-16 17:52:07 +0100

    Test: Add test for correct number of words generated with multiple digits provided

--- server/generateWordsFromNumbers.test.js
+++ server/generateWordsFromNumbers.test.js
@@ -10,7 +10,11 @@ describe('generateWordsFromNumbers function', () => {
     expect(generateWordsFromNumbers(['2'])).toEqual(['a', 'b', 'c']);
   })
 
-  test('returns correct array of letters if multiple number is provided', () => {
-    expect(generateWordsFromNumbers(['2', '3'])).toEqual(['ad', 'bd', 'cd', 'ae', 'be', 'ce', 'af', 'bf', 'cf']);
+  test('returns correct amount of letters if multiple number is provided', () => {
+    expect(generateWordsFromNumbers(['2', '3']).length).toEqual(9);
+  })
+
+  test('returns correct array of letters if multiple number is provided in alphabetical order', () => {
+    expect(generateWordsFromNumbers(['2', '3'])).toEqual(['ad', 'ad', 'ad', 'be', 'be', 'be', 'cf', 'cf', 'cf',]);
   })
 })
